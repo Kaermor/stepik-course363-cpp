bc71781ee672f418fc7ed8beafedec67cb60c1f4
Sergey Kovalev <kaermor@gmail.com>
2019-09-17 16:00:49 +0300

    Added first variant of task solution.

---
+++ 1-6-11z.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    double a, b, c, d, e, f, x, y;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+
+    x = (b*f - d*e)/(b*c - d*a)
+    y = (e - a*x)/b
+
+    cout << x << " " << y << endl;
+}
