c9b8fb90fb926c9d9b1298bc1621aecc8b4b1ce4
Sergey Kovalev <kaermor@gmail.com>
2019-09-19 11:02:56 +0300

    Added solution by Kramer's method (if accordingly task delta non equal zero).

--- 1-6-11z.cpp
+++ 1-6-11z.cpp
@@ -14,26 +14,9 @@ int main() {
     cin >> e;
     cin >> f;
 
-    if (b == 0 and a !=0){
-        x = e/a;
-        y = (f - c*x)/d;
-    }
-    else if (a == 0 and  b != 0){
-        y = e/b;
-        x = (x - d*y)/c;
-    }
-    else if (d == 0 and c != 0){
-        x = f/c;
-        y = (e - a*x)/b;
-    }
-    else if (c == 0 and d != 0){
-        y = f/d;
-        x = (e - b*y)/a;
-    }
-    else{
-        x = (b*f - d*e)/(b*c - d*a);
-        y = (e - a*x)/b;
-    }
+    x = (e*d - b*f) / (d*a - b*c);
+    y = (a*f - e*c) / (d*a - b*c);
+
  
     cout << x << " " << y << endl;
 }
