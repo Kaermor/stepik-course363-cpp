42e22657d0df8592593aa99cd5b33aaed464b56a
Sergey Kovalev <kaermor@gmail.com>
2019-09-19 10:57:28 +0300

    Addedsecond solution as if-else for division by zero.

--- 1-6-11z.cpp
+++ 1-6-11z.cpp
@@ -14,8 +14,26 @@ int main() {
     cin >> e;
     cin >> f;
 
-    x = (b*f - d*e)/(b*c - d*a);
-    y = (e - a*x)/b;
-
+    if (b == 0 and a !=0){
+        x = e/a;
+        y = (f - c*x)/d;
+    }
+    else if (a == 0 and  b != 0){
+        y = e/b;
+        x = (x - d*y)/c;
+    }
+    else if (d == 0 and c != 0){
+        x = f/c;
+        y = (e - a*x)/b;
+    }
+    else if (c == 0 and d != 0){
+        y = f/d;
+        x = (e - b*y)/a;
+    }
+    else{
+        x = (b*f - d*e)/(b*c - d*a);
+        y = (e - a*x)/b;
+    }
+ 
     cout << x << " " << y << endl;
 }
