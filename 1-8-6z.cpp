0bade81ac00cb4ff0dd9ed91349216ddc89869cd
Sergey Kovalev <kaermor@gmail.com>
2019-10-24 09:17:25 +0300

    1-8-6z. Solved.

--- 1-8-6z.cpp
+++ 1-8-6z.cpp
@@ -4,5 +4,28 @@ using namespace std;
 
 void foo_1_8_6z()
 {
+    int n, m, i1, j1, tmp;
+    int a[100][100];
+    cin >> n >> m;
+    //input
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> a[i][j];
+        }
 
+    }
+    cin >> i1 >> j1;
+    //processing
+    for (int i = 0; i < n; ++i) {
+        tmp = a[i][i1];
+        a[i][i1] = a[i][j1];
+        a[i][j1] = tmp;
+    }
+    //output
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << a[i][j] << " ";
+        }
+    cout << endl;
+    }
 }
